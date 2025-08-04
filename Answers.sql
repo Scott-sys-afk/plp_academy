f3a53badd96c0b212c8232399938e4129ccc9010
Paul Barasa <barasap828@gmail.com>
2025-08-04 22:52:05 +0300

    Add files via upload

---
+++ Answers.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE salesDB;
+/*
+Explanation:
+- SalesDB is the name we're giving to our new database
+*/
+DROP DATABASE demo;
+/*Explanation:
+- DROP DATABASE is the SQL command to delete a database
+*/
